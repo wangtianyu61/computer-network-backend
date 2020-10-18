9602c46d6f3cc7928130090e8df71ad0325ec832
wangtianyu61 <wangtianyu6162@gmail.com>
2020-10-18 12:18:58 +0800

    finish the book part

--- user/views_book.py
+++ user/views_book.py
@@ -8,6 +8,7 @@ from django.db import transaction
 from django.db import IntegrityError
 from django.db.models import Sum, Count, Max, Min, Avg
 from user.param import *
+import random
 
 #the sell gets the book of his own.
 def book_of(request, pk):
@@ -33,17 +34,33 @@ def add_book_entry(request):
     ## check the logic
     if book_info["original_price"] < 0:
         add_info["message"] = "The original price cannot be negative!"
-        return http.HttpResponse(add_info)
+        return http.JsonResponse(add_info)
     if book_info["price"] < 0:
         add_info["message"] = "The set price cannot be negative!"
-        return http.HttpResponse(add_info)
+        return http.JsonResponse(add_info)
+    if book_info["inventory"] < 0:
+        add_info["message"] = "The inventory cannot be negative!"
+        return http.JsonResponse(add_info)
     entry_images = dict_data['pictures']
     save_tag = transaction.savepoint()
     try:
         with transaction.atomic():
             del dict_data['pictures']
             ## add into the entry info
-            entry_info = Entry.objects.create(**dict_data)
+            entry_info = Entry()
+            entry_info.seller_id = dict_data['seller_id']
+            entry_info.name = dict_data['name']
+            entry_info.author = dict_data['author']
+            entry_info.category = dict_data['category']
+            entry_info.original_price = dict_data['original price']
+            entry_info.price = dict_data['price']
+            entry_info.description = dict_data['description']
+            entry_info.customer_inventory = dict_data['inventory']
+            entry_info.seller_inventory = dict_data['inventory']
+            #check for the manager
+            entry_info.status = 1
+            entry_info.save()
+            #entry_info = Entry.objects.create(**dict_data)
             ## add into the entry info
             for index in range(len(entry_images)):
                 new_entry_image = EntryImage()
@@ -80,18 +97,75 @@ def delete_book_entry(request):
 #update the book information
 @transaction.atomic
 def update_book_entry(request):
-    pass
+    book_update_permit = json.loads(request.body, strict = False)
+    update_info ={"success":0, "message":""}
+    if book_update_permit["price"] < 0:
+        update_info["message"] = "The set price cannot be negative!"
+        return http.JsonResponse(update_info)
+    if book_update_permit["update_number"] < 0:
+        update_info["message"] = "The inventory cannot be negative!"
+        return http.JsonResponse(update_info)
+    save_tag = transaction.savepoint()
+    #change the inventory
+    try:
+        with transaction.atomic():
+            update_entry = Entry.objects.get(entry_id = book_update_permit["entry_id"])
+            change_num = book_update_permit["update_number"] - update_entry.customer_inventory
+            if change_num + update_entry.seller_inventory < 0:
+                update_info["message"] = "The inventory cannot be negative!"
+                return http.JsonResponse(update_info)
+            update_entry.customer_inventory = book_update_permit["update_number"]
+            update_entry.seller_inventory = change_num + update_entry.seller_inventory
+            update_entry.price = book_update_permit["price"]
+            update_entry.description = book_update_permit["description"]
+            update_entry.save()
+            update_info["success"] = 1
+    except Exception as e:
+        update_info["message"] = "Update Error!"
+        transaction.savepoint_rollback(update_info)
+    return http.JsonResponse(update_info, strict = False)
 
 def book_summary(request):
     #randomly show 10 books on the main page
-    pass
+    available_book = Entry.objects.filter(customer_inventory__gte = 0)
+    available_entry_id = [entry['entry_id'] for entry in available_book]
+    random_entries = random.sample(available_entry_id, show_number)
+    book_info = []
+    for select_entry in random_entries:
+        entry_info = {"entry_id":select_entry.entry_id,
+                        "name":select_entry.name,
+                        "author":select_entry.author,
+                        "category":select_entry.category,
+                        "price":select_entry.price,
+                        "original_price":select_entry.original_price,
+                        "inventory":select_entry.customer_inventory}
+        book_info.append(entry_info)
+    return http.JsonResponse({"book_info":book_info}, strict = False)
 
 def book_detail(request, pk):
     #show the detail information of one book
-    pass
+    entry_info = Entry.objects.get(entry_id = pk)
+    entry_detail = {"entry_id":entry_info.entry_id, "name":entry_info.name, "author":entry_info.author,
+                    "price":entry_info.price, "original_price":entry_info.original_price,"category":entry_info.category,
+                    "description":entry_info.description, "inventory":entry_info.customer_inventory}
+    entry_images = EntryImage.objects.filter(entry_id = pk)
+    entry_detail["image"] = [entry_image.image for entry_image in entry_images]
+    entry_detail["entry_comment"] = []
+    entry_comments = EntryComment.objects.filter(entry_id = pk)
+    for comment_item in entry_comments:
+        comment_detail = {"comment_time":comment_item.comment_time, "entry_comment":comment_item.entry_comment,
+                            "entry_feedback":comment_item.entry_feedback}
+        entry_detail["entry_comment"].append(comment_detail)
+    return http.JsonResponse(entry_detail, strict = False)
+
 
 #search the book vaguely in the text
 def search_book(request):
+    #add into one type
+    pass
+
+#search the book by type:
+def search_book_type(request):
     pass
 
 
