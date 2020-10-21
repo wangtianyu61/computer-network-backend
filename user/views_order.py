ac681d1d42383e175a6c80557b4618b351b20feb
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-10-21 21:46:57 +0800

    Update views_order.py

--- user/views_order.py
+++ user/views_order.py
@@ -7,3 +7,18 @@ from django.db.models import Max
 from django.db import transaction
 from django.db import IntegrityError
 from django.db.models import Sum, Count, Max, Min, Avg
+
+
+
+def order_book(request):
+    print(request.body)
+    data = eval(str(request.body,encoding='utf-8'))
+    print(data)
+    customer_id = data['customer_id'] # customer id
+    order_info = data['order_detail']
+    entry_id = order_info[0] # entry id
+    order_number = order_info[1] # order amount
+
+    book = Entry.objects.get(entry_id=entry_id)
+    
+    pass
