d738729a1084667a6e3231cc12920cb50ed17c60
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-10-21 23:48:54 +0800

    Update views_order.py

--- user/views_order.py
+++ user/views_order.py
@@ -11,14 +11,30 @@ from django.db.models import Sum, Count, Max, Min, Avg
 
 
 def order_book(request):
+    bill_postage = 3
     print(request.body)
     data = eval(str(request.body,encoding='utf-8'))
     print(data)
     customer_id = data['customer_id'] # customer id
     order_info = data['order_detail']
-    entry_id = order_info[0] # entry id
-    order_number = order_info[1] # order amount
 
-    book = Entry.objects.get(entry_id=entry_id)
-    
+    result = {'success':1,'order_id':-1,'postage':-1}
+    for bill in order_info:
+        entry_id = bill[0]
+        order_amount = bill[1]
+        book = Entry.objects.get(entry_id=entry_id)
+        try:
+            inventory = book.customer_inventory
+        except:
+            result['success'] = 0
+        if inventory < order_amount: result['success'] = 0
+        if result['success'] == 0: break
+    if result['success'] == 1:
+        new_order = OrderInfo()
+        new_order.save()
+        result['order_id'] = new_order.order_id
+        result['postage'] = len(order_info) * bill_postage
+    return JsonResponse(result,safe=False,json_dumps_params={'ensure_ascii':False})
+
+def order_book_confirm(request):
     pass
